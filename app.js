ca4a5e030c238f5350fd21b05d6d6bee381cdcf2
Kosal Vann <kosal.vann@gmail.com>
2022-09-28 18:33:02 -0500

    added additional dependencies

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const app = express();
+const cors = require('cors')({origin: true});
+const Handlebars = require('handlebars');
+const hbs = require('express-handlebars');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
@@ -8,4 +14,12 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
 	console.log(`Server started at port ${port}`);
+});
+
+/** 
+ * @desc Catch-all for non-existing routes and pages
+ * @return {string}
+ */
+app.get('*', (req, res) => {
+	res.status(404).send('Not found');
 });
