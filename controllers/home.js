cd8ef8631ef8f83c636e58d3e0ded5f1ecf29fd5
Kosal Vann <kosal.vann@gmail.com>
2022-10-23 11:21:12 -0500

    Remove email function from home route

--- controllers/home.js
+++ controllers/home.js
@@ -2,15 +2,12 @@ const express = require("express");
 const router = express.Router();
 const bodyParser = require('body-parser');
 const translate = require('../helpers/locale');
-const sendEmail = require('../helpers/email');
 
 // Required by express-recaptcha to get data from body or query.
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
 router.get('/', async (req, res, next) => {
-	sendEmail('Kosal', process.env.ADMIN_EMAIL_ADDRESS, 'Yes, this is just a test. Disregard this test. Thanks.')
-
 	res.render('home', {
 		lang: translate[`${req.lang}`]
 	});
